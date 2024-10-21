4e92ea3a0072e7a90d5ca5d162f28b3988849abc
Amdadul Haque <amdadul1807107@gmail.com>
2024-10-21 15:19:01 +0600

    fix: footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { FaLinkedin, FaFacebookF } from 'react-icons/fa'
 import React from 'react'
 
 const Footer = () => {
@@ -17,28 +18,34 @@ const Footer = () => {
               className="h-16 sm:h-20 w-full"
             />
           </Link>
-
           {/* Address and Email */}
           <div className="text-center sm:text-left mt-6 sm:mt-0">
-            <p>1234 Business Avenue, Suite 500</p>
-            <p>Dhaka, Bangladesh</p>
-            <p>Email: <a href="mailto:info@hiringzen.com" className="underline">info@hiringzen.com</a></p>
+            <h3 className="text-lg font-semibold mb-2">Contact Us</h3>
+            <p>Dublin, Ireland</p>
+            <p>Email: <a href="mailto:hello@hiringzen.com" className="underline">hello@hiringzen.com</a></p>
           </div>
 
           {/* Social Media Links */}
-          <div className="mt-6 sm:mt-0 flex space-x-4">
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400">
-              <span className="sr-only">LinkedIn</span>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M4.98 3.5c0 1.38-1.12 2.5-2.5 2.5C1.12 6 0 4.88 0 3.5 0 2.12 1.12 1 2.5 1c1.38 0 2.5 1.12 2.5 2.5zM1 8h3.94v12H1V8zm6.01 0H11V9.68h.04c.55-1.02 1.88-2.1 3.86-2.1 4.13 0 4.88 2.72 4.88 6.26v7.16h-3.95v-6.33c0-1.51-.03-3.45-2.1-3.45-2.11 0-2.43 1.65-2.43 3.35v6.43H7V8z" />
-              </svg>
-            </a>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
-              <span className="sr-only">Facebook</span>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.406.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.894-4.788 4.659-4.788 1.325 0 2.463.099 2.794.143v3.24l-1.918.001c-1.504 0-1.796.715-1.796 1.763v2.311h3.59l-.467 3.622h-3.123V24h6.116c.73 0 1.325-.594 1.325-1.324V1.325C24 .593 23.406 0 22.675 0z" />
-              </svg>
-            </a>
+          <div className="mt-6 sm:mt-0 flex flex-col items-center sm:items-end space-y-4">
+            <h3 className="text-lg font-semibold">Find Us</h3>
+            <div className="flex space-x-4">
+              <Link
+                href="https://www.linkedin.com/company/hiringzen/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-400"
+              >
+                <FaLinkedin className="h-6 w-6" />
+              </Link>
+              <Link
+                href="https://facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-600"
+              >
+                <FaFacebookF className="h-6 w-6" />
+              </Link>
+            </div>
           </div>
         </div>
 
