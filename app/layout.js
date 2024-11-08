6ba0a755c66d75ab30b832271f72fe39ba782806
Amdadul Haque <amdadul1807107@gmail.com>
2024-11-08 15:19:23 +0600

    check

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,8 @@ export const metadata = {
   description: "Hiring Zen is a job board for the best companies in the world.",
 };
 
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{ scrollBehavior: "smooth" }}>
